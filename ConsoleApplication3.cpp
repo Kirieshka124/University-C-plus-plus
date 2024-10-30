14493c34b1ce6bc588e99fb853ff632e88ad6bb2
Kirieshka124 <artem_babynin@mail.ru>
2024-10-30 09:05:14 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <corecrt_math.h>
+#include <iostream>
+int main() {
+	float x=-1534, y=-9123;
+	const double PI = acos(-1.0);
+	double R = (cos(x * PI) / sin(x * PI)) - 1 / cos(y);
+	double S = tan(x) - log(abs((x * x * x) + (y * y * y)));
+	std::cout << R << std::endl;
+	std::cout << S << std::endl;
+	double C = std::max(S, R);
+	std::cout << S << std::endl;
+	return 0;
+}
